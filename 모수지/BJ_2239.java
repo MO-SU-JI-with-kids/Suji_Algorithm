dd4e04604dfdb8a2bccb0c2de2d10a0584624f73
SuJi Mo <108855492+nammsamm@users.noreply.github.com>
2026-02-06 09:54:55 +0900

    [모수지] week1 BJ_2239

--- 모수지/BJ_2239.java
+++ 모수지/BJ_2239.java
@@ -1,17 +1,14 @@
 import java.util.*;
 import java.io.*; // BufferedReader 사용을 위한 import
 
-// [모수지] week1 BJ_17413
+// [모수지] week1 BJ_2239
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int[][] info = new int[9][9];
     static int[][] visited_y = new int[9][10]; // 세로 방향 방문 체크
     static int[][] visited_x = new int[9][10]; // 가로 방향 방문 체크
-    
-    // 3*3 방문 체크
-    static int[][][] visited_box = new int[3][3][10];
-    
+    static int[][][] visited_box = new int[3][3][10]; // 3*3 방문 체크
 
     public static void solve(int y, int x) {
     	if(y == 9) {
@@ -25,12 +22,8 @@ public class Main {
     	
     	// 다음 칸으로 넘어가기
     	else if(info[y][x] != 0) {
-    		if(x ==8) {
-				solve(y+1, 0);
-			}
-			else {
-				solve(y, x+1);
-			}
+    		if(x ==8) solve(y+1, 0);
+			else solve(y, x+1);
     	}
     	
     	else {
@@ -47,12 +40,8 @@ public class Main {
         			canFill = true;
         			
         			// 다음 번호를 찾아 떠나요~
-        			if(x ==8) {
-        				solve(y+1, 0);
-        			}
-        			else {
-        				solve(y, x+1);
-        			}
+        			if(x ==8) solve(y+1, 0);
+        			else solve(y, x+1);	
 
         			visited_y[y][i]=0;  visited_x[x][i]=0; visited_box[y/3][x/3][i] =0;
         			info[y][x] = 0;// 사용 취소(백트래킹)
@@ -67,29 +56,6 @@ public class Main {
     	}
     }
 
-    public static boolean check() {
-    	boolean done = true;
-    	for(int i=0; i<9; i++) {
-    		for(int j=0; j<9; j++) {
-    			if(info[i][j] ==0) {
-    				done = false;
-    				break;
-    			}
-    		}
-    	}
-    	
-    	// 모든 칸을 채웠다면
-    	if(done == true) {
-    		for(int i=0; i<9; i++) {
-    			for(int j=0; j<9; j++) {
-    				System.out.print(info[i][j]);
-    			}System.out.println();
-    		}
-    		
-    	}	
-    	return done;
-    }
- 
     public static void main(String[] args) throws IOException {
     	int fillCnt = 0;
     	for(int i=0; i<9; i++) {
@@ -101,7 +67,6 @@ public class Main {
     			visited_box[i/3][j/3][in] = 1;
     		}
     	}
-    	
     	solve(0,0);
     }
 }
