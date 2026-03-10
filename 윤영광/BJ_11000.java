6d95bfb174c750209b280d3ac5dd2753599c69bb
윤영광 <66102223+glory-yun@users.noreply.github.com>
2026-03-10 10:17:13 +0900

    [윤영광] week6 BJ_11000

---
+++ 윤영광/BJ_11000.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+class BJ_11000 {
+	long s, t;
+
+	pair(long s, long t) {
+		this.s = s;
+		this.t = t;
+	}
+}
+
+public class Main {
+
+	static Scanner sc = new Scanner(System.in);
+	static int n, m, k, t;
+
+	public static void main(String[] args) {
+
+		n = sc.nextInt();
+
+		pair[] list = new pair[n];
+
+		for (int i = 0; i < n; i++) {
+			list[i] = new pair(sc.nextLong(), sc.nextLong());
+		}
+
+		Arrays.sort(list, (a, b) -> {
+
+			if (a.s == b.s) {
+				return (int) (a.t - b.t);
+			}
+
+			return (int) (a.s - b.s);
+		});
+
+		long end = list[0].t;
+
+		PriorityQueue<Long> pq = new PriorityQueue<>();
+		pq.add(end);
+
+		for (int i = 1; i < n; i++) {
+			pair now = list[i];
+
+			if (pq.peek() <= now.s) {
+				pq.poll();
+			}
+
+			pq.add(now.t);
+		}
+
+		System.out.println(pq.size());
+
+	}
+
+}
